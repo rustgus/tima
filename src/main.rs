ce506e694e6e775074f27948c63d7993594635c2
Gus Garcia <guscastles@gmail.com>
2021-08-27 06:52:34 +1000

    Add comments for rustdocs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+//! TimerIt - A simple timer in Rust
+//!
+//! Using the timer:
+//! ```
+//! timer <sec|min> [-m]
+//! ```
+//! where 5 is the number of seconds
+//! or minutes, with the `-m` option.
+
 fn main() {
     println!("Hello, world!");
 }
 
+/// Timer struct, with the `max_count` variable
+/// to be set with the number or seconds or minutes.
 #[derive(Debug)]
 pub struct Timer {
     max_count: u64,
 }
 
+/// Method implementation for the Timer struct.
 impl Timer {
+    /// `new` method implementation, with `max_count` mandatory.
     pub fn new(max_count: u64) -> Self {
         Timer {
             max_count: max_count,
@@ -17,8 +30,8 @@ impl Timer {
 
 #[cfg(test)]
 mod tests {
-    use std::env::args;
     use super::*;
+    use std::env::args;
 
     #[test]
     fn test_timer_new() {
