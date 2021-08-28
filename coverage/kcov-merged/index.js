400c6ad24dc9be3aa56ffb1be652ba6f973ce474
Gus Garcia <guscastles@gmail.com>
2021-08-28 10:42:24 +1000

    Add coverage report

---
+++ coverage/kcov-merged/index.js
@@ -0,0 +1,7 @@
+var data = {files:[
+{"link":"lib.rs.2a45fa2c.html","title":"lib.rs","summary_name":"[...]/timer/src/lib.rs","covered_class":"lineCov","covered":"100.0","covered_lines":"1","uncovered_lines":"0","total_lines" : "1"},
+{"link":"timer.rs.e4ca3f14.html","title":"timer.rs","summary_name":"[...]/timer/src/timer.rs","covered_class":"lineCov","covered":"91.5","covered_lines":"54","uncovered_lines":"5","total_lines" : "59"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2021-08-28 10:38:37", "instrumented" : 60, "covered" : 55,};
+var merged_data = [];
