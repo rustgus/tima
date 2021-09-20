e26a79780b2371d52ee1d68c1e51a4b0d5b46455
Gus Garcia <gusgarciapt@gmail.com>
2021-09-21 08:36:59 +1000

    Add macro for verbose (non-quiet) mode

--- tima-app/src/main.rs
+++ tima-app/src/main.rs
@@ -9,11 +9,39 @@
 use std::env;
 use tima_engine::engine::*;
 
+macro_rules! verbose {
+    ($tmr:ident $(,$txt:literal)?) => {
+        if !$tmr.quiet_mode {
+            $(
+                println!("{}: {} {}", $txt, $tmr.max_count, if $tmr.minutes { "minutes" } else { "seconds" });
+            )?
+        }
+    };
+}
+
 fn main() {
     let tmr = Tima::init(env::args().skip(1).collect());
-    if !tmr.quiet_mode {
-        println!("Tima started");
-    }
+    verbose!(tmr, "Tima started");
     tmr.start();
-    println!("Tima finished");
+    verbose!(tmr, "Tima finished");
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn verbose_with_quiet_mode() {
+        let mut tmr = Tima::new();
+        tmr.quiet_mode = true;
+        verbose!(tmr)
+    }
+
+    #[test]
+    fn verbose_without_quiet_mode() {
+        let mut tmr = Tima::new();
+        tmr.max_count = 10;
+        verbose!(tmr, "Tima started")
+    }
 }
